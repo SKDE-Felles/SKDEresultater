9e8c04d7edeb77839b648ab96c101758d038811a
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2019-10-23 12:51:47 +0200

    testdata: reduced size + trend (#5)
    * testdata: reduced size + trend
    
    - Went from 1000 to 500 points.
    - Lower UNN data last couple of months, to make trend
    
    * Update test

--- tests/dev/make_testdata.r
+++ tests/dev/make_testdata.r
@@ -1,25 +1,25 @@
 # Make testdata
 
-num <- 160
+num <- 80
 bo <- sample("Helgeland", size = num, replace = TRUE)
 dato <- sample(seq(as.Date("2017/01/01"), as.Date("2019/10/10"), by = "day"), num, replace = TRUE)
 verdi <- rpois(num, lambda = 32)
 helgeland <- data.frame(bohf = bo, dato = dato, tid_min = verdi)
 
-num <- 160
+num <- 80
 bo <- sample("Finnmark", size = num, replace = TRUE)
 dato <- sample(seq(as.Date("2017/01/01"), as.Date("2019/10/10"), by = "day"), num, replace = TRUE)
 verdi <- rnorm(num, mean = 30, sd = 20) * seq(1, num)
 verdi <- rpois(num, lambda = 25)
 finnmark <- data.frame(bohf = bo, dato = dato, tid_min = verdi)
 
-num <- 400
+num <- 200
 bo <- sample("UNN", size = num, replace = TRUE)
 dato <- sample(seq(as.Date("2017/01/01"), as.Date("2019/10/10"), by = "day"), num, replace = TRUE)
 verdi <- rpois(num, lambda = 40)
 unn <- data.frame(bohf = bo, dato = dato, tid_min = verdi)
 
-num <- 280
+num <- 140
 bo <- sample("Nordland", size = num, replace = TRUE)
 dato <- sample(seq(as.Date("2017/01/01"), as.Date("2019/10/10"), by = "day"), num, replace = TRUE)
 verdi <- rpois(num, lambda = 35)
@@ -27,7 +27,17 @@ nordland <- data.frame(bohf = bo, dato = dato, tid_min = verdi)
 
 testdata <- rbind(finnmark, helgeland, unn, nordland)
 
-median(testdata$tid_min)
+for (i in seq_len(length(testdata$tid_min))) {
+  if ((testdata$bohf[i] == "UNN") & (testdata$dato[i] > "2019-06-01") & (testdata$tid_min[i] > 5)) {
+    testdata$tid_min[i] <- testdata$tid_min[i] - 3
+  }
+  if ((testdata$bohf[i] == "UNN") & (testdata$dato[i] > "2019-05-01") & (testdata$tid_min[i] > 5)) {
+    testdata$tid_min[i] <- testdata$tid_min[i] - 3
+  }
+  if ((testdata$bohf[i] == "UNN") & (testdata$dato[i] > "2019-04-01") & (testdata$tid_min[i] > 5)) {
+    testdata$tid_min[i] <- testdata$tid_min[i] - 3
+  }
+}
 
 ggplot2::ggplot(data = testdata,
                 ggplot2::aes(x = dato, y = tid_min)) +
