39ba3f0fddd5251d67d2602e13e685ca079618e1
Arnfinn <arnfinn.steindal@gmail.com>
2019-11-01 13:25:33 +0100

    Plot polish
    - Removed dead code
    - Norwegian text
    - Removed some buttons
    - Changed ref-line to Mål and color to red
    - Changed Median to Median HN, and color to black

--- R/dotplot.r
+++ R/dotplot.r
@@ -9,13 +9,13 @@
 #'
 dotplot <- function(data_to_plot = NULL, all_data = NULL, ref_line = 30) {
   boomr <- unique(all_data$bohf)
-  farger <- SKDEr::skde_colors(num = 5)[seq_len(length(boomr))]
+  farger <- SKDEr::skde_colors(num = length(boomr))[seq_len(length(boomr))]
   names(farger) <- boomr
   ymax <- plyr::round_any(max(all_data$tid_min) + 1, 10, f = ceiling)
   xmin <- min(all_data$dato)
   xmax <- max(all_data$dato)
-  xmedian <- median(all_data[["tid_min"]])
-  xaxis <- list(title = "Dato",
+  xmedian <- median(all_data$tid_min)
+  xaxis <- list(title = "",
                 showline = FALSE,
                 showgrid = FALSE,
                 showticklabels = TRUE,
@@ -27,53 +27,29 @@ dotplot <- function(data_to_plot = NULL, all_data = NULL, ref_line = 30) {
                   buttons = list(
                     list(
                       count = 3,
-                      label = "3 mo",
+                      label = "3 mnd",
                       step = "month",
                       stepmode = "todate"),
                     list(
                       count = 6,
-                      label = "6 mo",
+                      label = "6 mnd",
                       step = "month",
                       stepmode = "todate"),
                     list(
                       count = 12,
-                      label = "1 yr",
+                      label = "1 år",
                       step = "month",
-                      stepmode = "todate"),
-                    list(
-                      count = 1,
-                      label = "YTD",
-                      step = "year",
-                      stepmode = "todate"),
-                    list(step = "all",
-                         stepmode = "todate"))),
+                      stepmode = "todate")
+                    )),
                 rangeslider = list(type = "date"))
   yaxis <- list(title = "Antall minutter",
-                showgrid = FALSE,
+                showgrid = TRUE,
                 zeroline = FALSE,
                 showline = FALSE,
                 showticklabels = TRUE,
                 autotick = TRUE,
                 range = c(0, ymax),
                 fixedrange = FALSE)
-  refl <- list(x = xmin,
-               y = ref_line,
-               xanchor = "right",
-               yanchor = "middle",
-               text = ~paste0("ref_line:", ref_line),
-               font = list(family = "Arial",
-                           size = 16,
-                           color = I("black")),
-               showarrow = FALSE)
-  xmed <- list(x = xmax,
-               y = xmedian,
-               xanchor = "right",
-               yanchor = "middle",
-               text = ~paste0("median:", xmedian),
-               font = list(family = "Arial",
-                           size = 16,
-                           color = I("red")),
-               showarrow = FALSE)
   data_to_plot %>%
     dplyr::filter(dplyr::between(dato, xmin, xmax)) %>%
     plotly::plot_ly(x = ~dato,
@@ -82,27 +58,27 @@ dotplot <- function(data_to_plot = NULL, all_data = NULL, ref_line = 30) {
                         colors = farger,
                         hoverinfo = "text",
                         text = ~paste0("</br>Boområde: ", bohf,
-                                       "</br>dato: ", dato,
-                                       "</br> min: ", tid_min
+                                       "</br>Dato: ", dato,
+                                       "</br>Tid til beh.: ", tid_min, " min"
                         )) %>%
     plotly::add_segments(x = xmin,
                          xend = xmax,
                          y = ref_line,
                          yend = ref_line,
                          hoverinfo = "text",
-                         text = ~paste0("</br> ref-line =", ref_line),
-                         name = "ref_line",
-                         color = I("black"),
+                         text = ~paste0("</br> Mål = ", ref_line, " min"),
+                         name = "Mål",
+                         color = I("red"),
                          showlegend = TRUE) %>%
     plotly::add_segments(x = xmin,
                          xend = xmax,
                          y = xmedian,
                          yend = xmedian,
                          hoverinfo = "text",
-                         text = ~paste0("</br> median =", xmedian),
+                         text = ~paste0("</br> Median HN = ", xmedian, " min"),
                          linetype = I("dash"),
-                         color = I("red"),
-                         name = "median",
+                         color = I("black"),
+                         name = "Median HN",
                          showlegend = TRUE) %>%
     plotly::layout(xaxis = xaxis,
                    yaxis = yaxis)
