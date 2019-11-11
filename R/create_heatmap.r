21b34d88f8b16d034e2ef3b9c1f0a32196d6ae01
arnfinn <arnfinn.steindal@gmail.com>
2019-11-11 13:47:18 +0100

    rm lintr warnings and CI error

--- R/create_heatmap.r
+++ R/create_heatmap.r
@@ -3,20 +3,24 @@
 #' @param data Data to be plotted
 #'
 #' @return A d3heatmap
+#' @importFrom rlang .data
 #' @export
 #'
 create_heatmap <- function(data = NULL) {
     if ("level3_name" %in% colnames(data)) {
-        tmp <- tidyr::unite(data, level1_name, level2_name, level3_name, col = "combined", sep = ": ")
+        tmp <- tidyr::unite(data, .data$level1_name, .data$level2_name, .data$level3_name, col = "combined", sep = ": ")
     } else if ("level2_name" %in% colnames(data)) {
-        tmp <- tidyr::unite(data, level1_name, level2_name, col = "combined", sep = ": ")
+        tmp <- tidyr::unite(data, .data$level1_name, .data$level2_name, col = "combined", sep = ": ")
     } else if ("level1_name" %in% colnames(data)) {
         tmp <- data
         tmp$combined <- data$level1_name
     }
 
-    spread_data <- tidyr::spread(tmp[, c("area", "area_name", "combined", "value")], key = combined, value = value)
-    spread_data <- dplyr::filter(spread_data, area != 8888)
+    spread_data <- tidyr::spread(tmp[, c("area", "area_name", "combined", "value")],
+                                 key = .data$combined,
+                                 value = .data$value
+                                 )
+    spread_data <- dplyr::filter(spread_data, .data$area != 8888)
     row.names(spread_data) <- spread_data$area_name
     spread_data$area_name <- NULL
     spread_data$area <- NULL
