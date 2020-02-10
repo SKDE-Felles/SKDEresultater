e31a7ddd7fa014ed3bc4f56d37b3e1a8f434a29b
Arnfinn <arnfinn.steindal@gmail.com>
2020-02-10 11:19:04 +0100

    rm git_version code
    Is not used anymore. Thus, dead code

--- R/launch_app.r
+++ R/launch_app.r
@@ -5,24 +5,19 @@
 #' @param publish_app If TRUE: deploy app to shinyapps.io (default = FALSE)
 #' @param name The appName of the deployed shiny application (default = "experimental")
 #' @param shiny_account Which shiny account on shinyapps.io (default = "skde")
-#' @param git_hash Current git sha1 hash
-#' @param github_repo Current github repository
 #'
 #' @export
 launch_app <- function(dataset = NULL,
                        title = NULL,
                        publish_app = FALSE,
                        name = "experimental",
-                       shiny_account = "skde",
-                       git_hash = NULL,
-                       github_repo = NULL) {
+                       shiny_account = "skde"
+                       ) {
 
   # Create a directory with all necessary data.
   shinydir <- SKDEr::create_appdir(app_data = dataset,
                                    webpage_title = title,
-                                   package = "SKDEresultater",
-                                   git_hash = git_hash,
-                                   github_repo = github_repo)
+                                   package = "SKDEresultater")
 
   # Run the app
   if (publish_app) {
--- tests/scripts/deploy.R
+++ tests/scripts/deploy.R
@@ -7,9 +7,6 @@ rsconnect::setAccountInfo(name   = Sys.getenv("shinyapps_name"),
                           secret = Sys.getenv("shinyapps_secret")
                           )
 
-git_hash <- Sys.getenv("TRAVIS_COMMIT")
-github_account <- Sys.getenv("TRAVIS_REPO_SLUG")
-
 if (Sys.getenv("TRAVIS_BRANCH") == "master") {
   app_name <- "SKDEresultater"
 } else {
@@ -19,7 +16,5 @@ if (Sys.getenv("TRAVIS_BRANCH") == "master") {
 SKDEresultater::launch_app(
   dataset = SKDEresultater::testdata,
   publish_app = TRUE,
-  name = app_name,
-  git_hash = git_hash,
-  github_repo = github_account
+  name = app_name
 )
